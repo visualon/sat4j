daa466863ba345096400b4b90935478b2693be92
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2018-11-11 22:47:44 +0100

    Yet another sonarqube violation fix.

--- org.sat4j.core/src/main/java/org/sat4j/tools/SpeedTracing.java
+++ org.sat4j.core/src/main/java/org/sat4j/tools/SpeedTracing.java
@@ -94,8 +94,8 @@ public class SpeedTracing extends SearchListenerAdapter<ISolverService> {
     public void cleaning() {
         this.end = System.currentTimeMillis();
         long indexClean = this.index + this.end - this.begin;
-        this.visuTool.addPoint(indexClean / 1000.0, this.counter
-                / (this.end - this.begin) * 1000);
+        this.visuTool.addPoint(indexClean / 1000.0,
+                (this.counter * 1000.0) / (this.end - this.begin));
         this.cleanVisuTool.addPoint(indexClean / 1000.0, this.maxY);
         this.restartVisuTool.addInvisiblePoint(indexClean, 0);
     }
@@ -104,7 +104,7 @@ public class SpeedTracing extends SearchListenerAdapter<ISolverService> {
     public void restarting() {
         this.end = System.currentTimeMillis();
         long indexRestart = this.index + this.end - this.begin;
-        double y = this.counter / (this.end - this.begin) * 1000;
+        double y = (this.counter * 1000.0) / (this.end - this.begin);
         this.visuTool.addPoint(indexRestart / 1000.0, y);
         if (y > this.maxY) {
             this.maxY = y;
