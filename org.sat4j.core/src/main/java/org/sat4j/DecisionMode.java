49670f68c1fa3fee55c97283a3666870f1bd158e
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2019-06-13 16:06:33 +0200

    Fix for NPE found by Emmanuel.

--- org.sat4j.core/src/main/java/org/sat4j/DecisionMode.java
+++ org.sat4j.core/src/main/java/org/sat4j/DecisionMode.java
@@ -63,7 +63,7 @@ final class DecisionMode implements ILauncherMode {
             out.println(ANSWER_PREFIX + exitCode);
             if (exitCode != ExitCode.UNKNOWN
                     && exitCode != ExitCode.UNSATISFIABLE) {
-                int[] model = problem.model();
+                int[] model = solver.model();
                 if (System.getProperty("prime") != null) {
                     int initiallength = model.length;
                     logger.log("returning a prime implicant ...");
