f6902cb81a9c809a3d632c6a4cc65663de572a7c
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2021-02-10 14:43:40 +0100

    First version of the discerning solver (1 single solution).

--- org.sat4j.core/src/main/java/org/sat4j/Anthony.java
+++ org.sat4j.core/src/main/java/org/sat4j/Anthony.java
@@ -1,17 +1,56 @@
 package org.sat4j;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
+import org.sat4j.core.LiteralsUtils;
+import org.sat4j.core.VecInt;
 import org.sat4j.minisat.SolverFactory;
+import org.sat4j.minisat.core.ICDCL;
+import org.sat4j.minisat.core.ILits;
+import org.sat4j.minisat.core.IOrder;
+import org.sat4j.minisat.core.Solver;
 import org.sat4j.reader.DimacsReader;
 import org.sat4j.reader.ParseFormatException;
 import org.sat4j.reader.Reader;
 import org.sat4j.specs.ContradictionException;
 import org.sat4j.specs.ISolver;
+import org.sat4j.specs.IVecInt;
+import org.sat4j.specs.IteratorInt;
 import org.sat4j.specs.TimeoutException;
 import org.sat4j.tools.Backbone;
 
 public class Anthony {
+
+    static class Decision {
+        private final Integer d;
+        private final IVecInt propagations;
+
+        Decision(Integer d, IVecInt propagations) {
+            this.d = d;
+            this.propagations = propagations;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + d + "|" + propagations + ")";
+        }
+    }
+
+    private static IVecInt findPropagated(IVecInt implied,
+            IVecInt assumptions) {
+        IVecInt propagated = new VecInt();
+        for (IteratorInt it = implied.iterator(); it.hasNext();) {
+            int l = it.next();
+            if (!assumptions.contains(l)) {
+                propagated.push(l);
+                assumptions.push(l);
+            }
+        }
+        return propagated;
+    }
+
     public static void main(String[] args) throws TimeoutException,
             ParseFormatException, IOException, ContradictionException {
         if (args.length == 1) {
@@ -19,7 +58,27 @@ public class Anthony {
             ISolver solver = SolverFactory.newDefault();
             Reader reader = new DimacsReader(solver);
             reader.parseInstance(filename);
-            System.out.println(Backbone.instance().compute(solver));
+            IVecInt implied = Backbone.instance().compute(solver);
+            ICDCL<?> cdcl = (ICDCL<?>) solver;
+            ILits lits = ((Solver) solver).getVocabulary();
+            IOrder order = cdcl.getOrder();
+            IVecInt assumptions = new VecInt();
+            Collection<Decision> decisions = new ArrayList<>();
+            IVecInt propagated = findPropagated(implied, assumptions);
+            decisions.add(new Decision(null, propagated));
+            do {
+                for (IteratorInt it = implied.iterator(); it.hasNext();) {
+                    int l = it.next();
+                    lits.satisfies(LiteralsUtils.toInternal(l));
+                }
+                int d = LiteralsUtils.toDimacs(order.select());
+                assumptions.push(d);
+                implied = Backbone.instance().compute(solver, assumptions);
+                propagated = findPropagated(implied, assumptions);
+                decisions.add(new Decision(d, propagated));
+            } while (assumptions.size() < solver.realNumberOfVariables());
+            System.out.println(decisions);
         }
     }
+
 }
