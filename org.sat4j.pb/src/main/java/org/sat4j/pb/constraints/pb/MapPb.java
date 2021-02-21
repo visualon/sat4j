62a414b14e08b83113f74ede5beb040d01349d22
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2021-02-21 09:29:48 +0100

    Uses a default void PBSearchListener when MapPb are not used for conflict analysis (e.g. constraints normalization).

--- org.sat4j.pb/src/main/java/org/sat4j/pb/constraints/pb/MapPb.java
+++ org.sat4j.pb/src/main/java/org/sat4j/pb/constraints/pb/MapPb.java
@@ -36,6 +36,7 @@ import org.sat4j.minisat.constraints.cnf.Lits;
 import org.sat4j.minisat.core.VarActivityListener;
 import org.sat4j.pb.IPBSolverService;
 import org.sat4j.pb.tools.PBSearchListener;
+import org.sat4j.pb.tools.VoidPBTracing;
 import org.sat4j.specs.IVec;
 import org.sat4j.specs.IVecInt;
 
@@ -52,7 +53,7 @@ public class MapPb implements IDataStructurePB {
      */
     protected InternalMapPBStructure weightedLits;
 
-    protected PBSearchListener<IPBSolverService> listener;
+    protected PBSearchListener<IPBSolverService> listener = new VoidPBTracing();
 
     protected BigInteger degree;
 
