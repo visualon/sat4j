3ba5b56da5a173cf7573c18c4eb5df5456b8fe41
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2024-02-29 08:42:24 +0100

    :test_tube: test case for bug #175

---
+++ org.sat4j.core/src/test/java/org/sat4j/tools/BugSAT175.java
@@ -0,0 +1,39 @@
+package org.sat4j.tools;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.sat4j.minisat.SolverFactory;
+import org.sat4j.minisat.core.Counter;
+import org.sat4j.reader.LecteurDimacs;
+import org.sat4j.reader.ParseFormatException;
+import org.sat4j.specs.ContradictionException;
+import org.sat4j.specs.TimeoutException;
+
+public class BugSAT175 {
+
+    @Test
+    public void test() throws ParseFormatException, IOException,
+            ContradictionException, TimeoutException {
+        var satSolver = SolverFactory.newDefault();
+        var reader = new LecteurDimacs(satSolver);
+        var p = reader.parseInstance("src/test/testfiles/bug175.cnf");
+        var counter = new Counter();
+        SolutionFoundListener sfl = new SolutionFoundListener() {
+
+            @Override
+            public void onSolutionFound(int[] solution) {
+                counter.inc();
+            }
+
+        };
+        var enumerator = new SearchEnumeratorListener(sfl);
+        satSolver.setSearchListener(enumerator);
+        assertTrue(satSolver.isSatisfiable());
+        assertEquals(2, enumerator.getNumberOfSolutionFound());
+    }
+
+}
