3afec4a554e904c9df4e939b3506956958da66f9
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2024-05-21 21:27:36 +0200

    Updated test case producing both icnf and idrup proof.

--- org.sat4j.core/src/test/java/org/sat4j/tools/BugSAT176.java
+++ org.sat4j.core/src/test/java/org/sat4j/tools/BugSAT176.java
@@ -16,7 +16,10 @@ public class BugSAT176 {
     public void testKatalinExample()
             throws ContradictionException, TimeoutException {
         ISolver solver = SolverFactory.newDefault();
-        solver.setSearchListener(new IdrupSearchListener<>("/tmp/test.idrup"));
+        var idrup = new IdrupSearchListener<>("/tmp/test.idrup");
+        var icnf = new ICnfSearchListener<>("/tmp/test.icnf");
+        var both = new MultiTracing<>(icnf, idrup);
+        solver.setSearchListener(both);
         solver.addClause(VecInt.of(-1, 3, 4));
         solver.addClause(VecInt.of(-1, 3, -4));
         solver.addClause(VecInt.of(-2, -3, 4));
