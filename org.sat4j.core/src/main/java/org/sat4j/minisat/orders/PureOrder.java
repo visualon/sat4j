6e8225eb94e8c44b489eddf66b9e7d86bdbf1822
Daniel Le Berre <daniel.leberre@univ-artois.fr>
2024-06-18 18:21:45 +0200

    Fix for #179

--- org.sat4j.core/src/main/java/org/sat4j/minisat/orders/PureOrder.java
+++ org.sat4j.core/src/main/java/org/sat4j/minisat/orders/PureOrder.java
@@ -78,7 +78,8 @@ public final class PureOrder extends VarOrderHeap {
             this.cpt = 0;
             int nblits = 2 * this.lits.nVars();
             for (var i = 2; i <= nblits; i++) {
-                if (this.lits.isUnassigned(i) && this.lits.watches(i).size() > 0
+                if (this.lits.belongsToPool(i >> 1) && this.lits.isUnassigned(i)
+                        && this.lits.watches(i).size() > 0
                         && this.lits.watches(i ^ 1).size() == 0) {
                     return i;
                 }
